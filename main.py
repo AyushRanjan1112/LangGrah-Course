9cc8bdaf8e34c860f4c3ccf4a4d439ba4c02bab6
Ayush Ranjan <157922131+AyushRanjan1112@users.noreply.github.com>
2026-08-10 19:09:51 +0530

    complete reflection agent

--- main.py
+++ main.py
@@ -1,6 +1,71 @@
-def main():
-    print("Hello from langgrah-course!")
+from typing import TypedDict, Annotated
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, StateGraph
+from langgraph.graph.message import add_messages
+from chains import generate_chain, reflect_chain
+
+# generation_chain
+# reflect_chain
+
+
+class MessageGraph(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+# names of the nodes in the graph
+REFLECT = "reflect"
+GENERATE = "generate"
+
+# nodes in the graph
+def generation_node(state: MessageGraph):
+    return {"messages": [generate_chain.invoke({"messages": state["messages"]})]}
+
+# nodes in the graph
+def reflection_node(state: MessageGraph):
+    res = reflect_chain.invoke({"messages": state["messages"]})
+    return {"messages": [HumanMessage(content=res.content)]}
+
+
+builder = StateGraph(state_schema=MessageGraph)
+builder.add_node(GENERATE, generation_node)
+builder.add_node(REFLECT, reflection_node)
+builder.set_entry_point(GENERATE)
+
+def should_continue(state: MessageGraph):
+    if len(state["messages"]) > 6:
+        return END
+    return REFLECT
+
+builder.add_conditional_edges(GENERATE, should_continue, path_map={END: END, REFLECT: REFLECT})
+builder.add_edge(REFLECT, GENERATE)
+
+graph = builder.compile()
+print(graph.get_graph().draw_mermaid())
+
+# prints graph in ascii format
+# graph.get_graph().print_ascii()
 
 
 if __name__ == "__main__":
-    main()
+    print("Hello from langgrah-course!")
+    inputs = {
+        "messages": [
+            HumanMessage(
+                content="""Make this tweet better:"
+                                    @LangChainAI
+            — newly Tool Calling feature is seriously underrated.
+
+            After a long wait, it's  here- making the implementation of agents across different models with function calling - super easy.
+
+            Made a video covering their newest blog post
+
+                                  """
+            )
+        ]
+    }
+    response = graph.invoke(inputs)
+    print(response)
